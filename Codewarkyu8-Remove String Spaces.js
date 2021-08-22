05820d2df7be3063914ff275d389e3151baa2c32
LIZIHRONG <asbkk3697@gmail.com>
2021-08-22 20:27:22 +0800

    0822 commit

---
+++ Codewarkyu8-Remove String Spaces.js
@@ -0,0 +1,15 @@
+// Even or Odd
+/* 
+
+Description:
+Simple, remove the spaces from the string, then return the resultant string.
+
+*/
+
+// My solution:
+
+function noSpace(x) {
+  let x1 = x.split(" ");
+  console.log(x1);
+  return x1.join("");
+}
